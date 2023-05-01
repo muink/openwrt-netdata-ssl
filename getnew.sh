cb66c35e35e09c81262587dad68f50c477e5c828
Anya Lin <hukk1996@gmail.com>
2023-05-01 10:56:52 +0800

    fix update script

--- getnew.sh
+++ getnew.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git remote add packages https://github.com/openwrt/packages.git
 git fetch packages
-git branch --track netdata refs/remotes/packages/master
-git checkout netdata -- admin/netdata/
+#git branch --track netdata refs/remotes/packages/master
+git checkout packages/master -- admin/netdata/
 git diff-index --cached --quiet HEAD admin/netdata/ \
 || (git commit -m "Update netdata-nossl" && git push)

81e2e97279c8ff537258f93744a18df510970a47
Anya Lin <hukk1996@gmail.com>
2023-05-02 00:51:00 +0800

    mv

--- getnew.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-v='22.03'
-git remote add packages https://github.com/openwrt/packages.git
-git fetch packages
-#git branch --track netdata refs/remotes/packages/openwrt-$v
-git checkout packages/openwrt-$v -- admin/netdata/
-git diff-index --cached --quiet HEAD admin/netdata/ \
-|| (git commit -m "Update netdata-nossl $v" && git push)
